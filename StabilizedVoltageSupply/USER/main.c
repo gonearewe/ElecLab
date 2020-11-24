df11cfa5bbb3582368f4513b60c3f1fb3a269215
gonearewe <cristmactavish@outlook.com>
2020-11-24 15:09:59 +0800

    ready to use

--- StabilizedVoltageSupply/USER/main.c
+++ StabilizedVoltageSupply/USER/main.c
@@ -39,14 +39,9 @@ int main(void)
 	Adc_Init();										//ADC初始化
 	Dac1_Init();									//DAC初始化
 
-	POINT_COLOR = RED; //设置字体为红色
-	LCD_ShowString(60, 130, 200, 16, 16, "WK_UP: ON/OFF");
-	//显示提示信息
-	POINT_COLOR = BLUE; //设置字体为蓝色
-	LCD_ShowString(60, 150, 200, 16, 16, "DAC VAL: ");
-	LCD_ShowString(60, 170, 200, 16, 16, "DAC VOL: 0.000V");
-	LCD_ShowString(60, 190, 200, 16, 16, "ADC VOL: 0.000V");
-	LCD_ShowString(60, 210, 200, 16, 16, "Current: 0.00mA");
+	POINT_COLOR = RED;									   //设置字体为红色
+	LCD_ShowString(60, 130, 200, 16, 16, "WK_UP: ON/OFF"); //显示提示信息
+
 	// (1.23/R1 + (1.23-DAC_V)/R3) * R2 + 1.23 == 5
 	// DAC_V = DATA * (3.3 / 4096)
 	// DATA = DAC_V / 3.3 * 4096
@@ -77,22 +72,31 @@ int main(void)
 		}
 		if (t == 0 || key == KEY0_PRES || key == KEY1_PRES || key == WKUP_PRES) //按键按下了,或者定时时间到了
 		{
-			u16 tmp = DAC_GetDataOutputValue(DAC_Channel_1); //读取前面设置DAC的值
-			LCD_ShowxNum(124, 150, tmp, 4, 16, 0);			 //显示DAC寄存器值
-
+			POINT_COLOR = BLUE; //设置字体为蓝色
 			char display_buf[25] = {0};
+
+			u16 tmp = DAC_GetDataOutputValue(DAC_Channel_1); //读取前面设置DAC的值
+			sprintf(display_buf, "DAC VAL: %d", tmp);
+			LCD_ShowString(60, 150, 200, 16, 16, (u8 *)display_buf); //显示DAC寄存器值
+			clear(display_buf, 25);
+
 			sprintf(display_buf, "DAC VOL: %5.3lfV", tmp * (3.3 / 4096)); //显示DAC电压值
 			LCD_ShowString(60, 170, 200, 16, 16, (u8 *)display_buf);
 			clear(display_buf, 25);
 
-			tmp = Get_Adc_Average(ADC_Channel_1, 10);		  //得到ADC转换值
-			double adc_v = tmp * (3.3 / 4096) * 1000;		  // measured by mV
-			sprintf(display_buf, "ADC VOL: %5.3lfmV", adc_v); //显示ADC电压值
+			tmp = Get_Adc_Average(ADC_Channel_1, 10);	//得到ADC转换值
+			sprintf(display_buf, "ADC VAL: %d  ", tmp); //显示ADC电压值
 			LCD_ShowString(60, 190, 200, 16, 16, (u8 *)display_buf);
 			clear(display_buf, 25);
 
-			sprintf(display_buf, "Current: %6.2lfmA", adc_v / adc_r);
+			double adc_v = tmp * (3.3 / 4096) * 1000;		  // measured by mV
+			sprintf(display_buf, "ADC VOL: %5.3lfmV", adc_v); //显示ADC电压值
 			LCD_ShowString(60, 210, 200, 16, 16, (u8 *)display_buf);
+			clear(display_buf, 25);
+
+			POINT_COLOR = MAGENTA;
+			sprintf(display_buf, "Current: %6.2lfmA", adc_v / adc_r);
+			LCD_ShowString(60, 230, 200, 16, 16, (u8 *)display_buf);
 			clear(display_buf, 25);
 		}
 		delay_ms(10);
