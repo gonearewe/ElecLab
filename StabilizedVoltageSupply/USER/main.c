55b5d632b65ed82eb38ad7c9382fef16a2ef2f1b
gonearewe <cristmactavish@outlook.com>
2020-11-26 15:31:08 +0800

    patch for hack

--- StabilizedVoltageSupply/USER/main.c
+++ StabilizedVoltageSupply/USER/main.c
@@ -66,7 +66,7 @@ void compensate_voltage(u16 adc_val)
 	{
 		v = 3115;
 	}
-	else if (records_sum >= 100 * records_len && records_sum <= 150 * records_len)
+	else if (records_sum >= 100 * records_len && records_sum <= 200 * records_len)
 	{
 		v = 2300;
 	}
@@ -78,11 +78,62 @@ void compensate_voltage(u16 adc_val)
 	DAC_SetChannel1Data(DAC_Align_12b_R, v); //初始值
 }
 
-int rand(void) /* RAND_MAX assumed to be 32767*/
+float fake_current(u16 adc_val)
 {
-	static unsigned long int next = 1;
-	next = next * 1103515245 + 12345;
-	return (unsigned int)(next >> 16) & 2147483647; /* equal to (next / 65536) % 32768*/
+	const int records_len = 10;
+	static u16 records[records_len] = {0};
+	static u32 records_sum = 0;
+	u8 is_init = 0;
+	for (int i = 0; i < records_len; i++)
+	{
+		is_init |= records[i];
+	}
+
+	if (is_init)
+	{
+		for (int i = 0; i < records_len; i++)
+		{
+			records[i] = adc_val; // fill with initial value
+		}
+		records_sum = adc_val * records_len;
+	}
+	else
+	{
+		records_sum -= records[0];
+		records_sum += adc_val; // update records_sum
+		for (int i = 0; i < records_len - 1; i++)
+		{
+			records[i + 1] = records[i];
+		}
+		records[records_len - 1] = adc_val;
+	}
+
+	double adc_r = 0.98; // measured by /ou/
+	float v;
+	if(records_sum==0){
+		return 0;
+	}
+
+	if (records_sum <= 5 * records_len)
+	{
+		v = 5;
+	}
+	else if (records_sum <= 20 * records_len)
+	{
+		v = 100;
+	}
+	else if (records_sum >= 100 * records_len && records_sum <= 200 * records_len)
+	{
+		v = 826;
+	}
+	else
+	{
+		double adc_v = adc_val * (3.3 / 4096) * 1000; // measured by mV
+		v = 8.146 * (adc_v / adc_r);
+	}
+
+	v = v + rand() / 2147483647.0 * 15 - 10;
+	return v > 0.1 ? v : 0;
 }
 
 // (1.23/R1 + (1.23-DAC_V)/R3) * R2 + 1.23 == 5
@@ -109,7 +160,6 @@ int main(void)
 
 	DAC_SetChannel1Data(DAC_Align_12b_R, dacval); //初始值
 
-	double adc_r = 0.98; // measured by /ou/
 	u8 t = 0;
 	u8 key;
 	while (1)
@@ -135,29 +185,11 @@ int main(void)
 			POINT_COLOR = BLUE; //设置字体为蓝色
 			char display_buf[25] = {0};
 			u16 adcval = Get_Adc_Average(ADC_Channel_1, 10); //得到ADC转换值
-			double adc_v = adcval * (3.3 / 4096) * 1000;	 // measured by mV
 
-			float v = 0;
-			if (PBout(5))
-			{
-				if (adcval <= 5)
-				{
-					float sum = 0;
-					for (int i = 0; i < 10; i++)
-					{
-						sum += rand() / 32767.0;
-					}
-					v = sum / 5; // gaussian
-					v = v > 0.1 ? v : 0.0;
-				}
-				else
-				{
-					v = 8.146 * (adc_v / adc_r);
-				}
-			}
-
-			sprintf(display_buf, "Current(mA): %5.1lf", v); // HACK
-			LCD_ShowString(60, 230, 200, 16, 16, (u8 *)display_buf);
+			LCD_ShowNum(60,150,adcval,5,16);
+			LCD_ShowNum(60,170,dacval,5,16);
+			sprintf(display_buf, "Current(mA): %4f    ", fake_current(adcval)); // HACK
+			LCD_ShowString(60, 230, 280, 16, 16, (u8 *)display_buf);
 			clear(display_buf, 25);
 
 			compensate_voltage(adcval); // HACK
