047e5d973125df9389c65b38c1e67a9144a98d28
gonearewe <cristmactavish@outlook.com>
2020-12-18 00:18:46 +0800

    add fire display matrix

---
+++ FireMeasurement/USER/fire.h
@@ -0,0 +1,10 @@
+#ifndef __FIRE_H_
+#define __FIRE_H_
+
+#include "stm32f10x.h"
+
+const u16 outline[500][16];
+const u16 outer_fill[500][8];
+const u16 inner_fill[500][4];
+
+#endif
--- FireMeasurement/USER/main.c
+++ FireMeasurement/USER/main.c
@@ -3,9 +3,11 @@
 #include "key.h"
 #include "sys.h"
 #include "lcd.h"
-#include "usart.h"	 
+#include "usart.h"
 #include "adc.h"
- 
+#include "timer.h"
+#include "fire.h"
+
 /************************************************
  ALIENTEK精英STM32开发板实验17
  ADC 实验   
@@ -16,39 +18,40 @@
  作者：正点原子 @ALIENTEK
 ************************************************/
 
-   
- int main(void)
- {	 
-  u16 adcx;
+int main(void)
+{
+	u16 adcx;
 	float temp;
-	delay_init();	    	 //延时函数初始化	  
-	NVIC_PriorityGroupConfig(NVIC_PriorityGroup_2);//设置中断优先级分组为组2：2位抢占优先级，2位响应优先级
-	uart_init(115200);	 	//串口初始化为115200
- 	LED_Init();			     //LED端口初始化
-	LCD_Init();			 	
- 	Adc_Init();		  		//ADC初始化
+	delay_init();									//延时函数初始化
+	NVIC_PriorityGroupConfig(NVIC_PriorityGroup_2); //设置中断优先级分组为组2：2位抢占优先级，2位响应优先级
+	uart_init(115200);								//串口初始化为115200
+	LED_Init();										//LED端口初始化
+	LCD_Init();
+	TIM3_Int_Init(99, 7199); // 100Hz
+	Adc_Init();				 //ADC初始化
 
-	POINT_COLOR=RED;//设置字体为红色 
-	LCD_ShowString(60,50,200,16,16,"Elite STM32");	
-	LCD_ShowString(60,70,200,16,16,"ADC TEST");	
-	LCD_ShowString(60,90,200,16,16,"ATOM@ALIENTEK");
-	LCD_ShowString(60,110,200,16,16,"2015/1/14");	
-	//显示提示信息
-	POINT_COLOR=BLUE;//设置字体为蓝色
-	LCD_ShowString(60,130,200,16,16,"ADC_CH0_VAL:");	      
-	LCD_ShowString(60,150,200,16,16,"ADC_CH0_VOL:0.000V");	       
-	while(1)
-	{
-		adcx=Get_Adc_Average(ADC_Channel_1,10);
-		LCD_ShowxNum(156,130,adcx,4,16,0);//显示ADC的值
-		temp=(float)adcx*(3.3/4096);
-		adcx=temp;
-		LCD_ShowxNum(156,150,adcx,1,16,0);//显示电压值
-		temp-=adcx;
-		temp*=1000;
-		LCD_ShowxNum(172,150,temp,3,16,0X80);
-		LED0=!LED0;
-		delay_ms(250);	
+	POINT_COLOR = BLUE; //设置字体为蓝色
+	LCD_ShowString(60, 130, 200, 16, 16, "ADC_CH0_VAL:");
+	LCD_ShowString(60, 150, 200, 16, 16, "ADC_CH0_VOL:0.000V");
+	int x=50,y=200;
+	for(int i=0;i<500;i++){
+		for(int j=0,c=outline[i][j];c!=0;j++,c=outline[i][j]){
+			LCD_Fast_DrawPoint(x+c,y+i,RED);
+		}
 	}
- }
+	while (1)
+	{
+		adcx = Get_Adc_Average(ADC_Channel_1, 10);
+		LCD_ShowxNum(156, 130, adcx, 4, 16, 0); //显示ADC的值
+		temp = (float)adcx * (3.3 / 4096);
+		adcx = temp;
+		LCD_ShowxNum(156, 150, adcx, 1, 16, 0); //显示电压值
+		temp -= adcx;
+		temp *= 1000;
+		LCD_ShowxNum(172, 150, temp, 3, 16, 0X80);
+		LED0 = !LED0;
+		delay_ms(250);
+	}
+}
+
 
